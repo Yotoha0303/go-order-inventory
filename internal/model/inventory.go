3619eeed53245eab867dd56b41e56e2812bef05d
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2026-05-07 17:12:04 +0800

    update table struct

--- internal/model/inventory.go
+++ internal/model/inventory.go
@@ -4,7 +4,7 @@ import "time"
 
 type Inventory struct {
 	ID            int64     `gorm:"primaryKey;autoIncrement;type:bigint" json:"id"`
-	ProductID     int64     `gorm:"type:bigint;not null;uniqueIndex" json:"product_id"`
+	ProductID     int64     `gorm:"type:bigint;not null;uniqueIndex:uk_inventory_product_id" json:"product_id"`
 	StockQuantity int64     `gorm:"type:bigint;not null;default:0;check:stock_quantity >= 0" json:"stock_quantity"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
