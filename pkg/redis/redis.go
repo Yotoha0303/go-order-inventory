554176b2babd586dd7dbf0663944f462bea9f095
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2026-05-18 23:03:48 +0800

    fix bug

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -28,7 +28,7 @@ func InitRedis(cfg config.RedisConfig) (*redis.Client, error) {
 	defer cancel()
 
 	if err := client.Ping(ctx).Err(); err != nil {
-		return nil, fmt.Errorf("failed to connect to redis: %v", err)
+		return nil, fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
 	return client, nil
